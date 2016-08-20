6b65cb1a7ccd532bdb9dacc9eff973fad153f70e
Steve Witman <stevewitman@gmail.com>
2016-08-19 23:51:09 -0600

    watch a file for changes

--- configuration.js
+++ configuration.js
@@ -3,7 +3,7 @@ console.log("Starting ...");
 var contents = fs.readFileSync("files/config.json")
 console.log("contents: " + contents);
 var config = JSON.parse(contents);
-console.log("config: " + config);
+console.log("config: ",  config);
 console.log("username: " + config.username);
 
 // execute rest of code after reading a config file synchronously
---
+++ watch.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+console.log("started...");
+var config = JSON.parse(fs.readFileSync("files/config.json"));
+console.log('initial config: ', config);
+
+fs.watch("files/config.json", function(current, previous) {
+  console.log("config changed");
+  config = JSON.parse(fs.readFileSync("files/config.json"));
+  console.log("new config file: ", config);
+}
+)
