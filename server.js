882f87bf38d7a4fb071a01a785dad06bce9fb868
Steve Witman <stevewitman@gmail.com>
2016-08-23 00:40:35 -0600

    watch config file changes

--- server.js
+++ server.js
@@ -19,3 +19,12 @@ var server = http.createServer(function(request, response) {
 server.listen(port, host, function() {
   console.log("Listening " + host + ":" + port);
 });
+fs.watchFile("config.json", function() {
+  host = config.host;
+  port = config.port;
+  config = JSON.parse(fs.readFileSync("config.json"));
+  server.close();
+  server.listen(port, host, function() {
+    console.log("Now listening " + host + ":" + port);
+  });
+})
