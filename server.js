5ac13cf5647aaf18d7829251724b41f11c622847
Steve Witman <stevewitman@gmail.com>
2016-08-22 23:51:07 -0600

    added basic routing

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var http = require("http");
-
+var fs = require("fs");
 console.log("Starting ...");
 var host = "127.0.0.1";
 var port = 1337;
 var server = http.createServer(function(request, response) {
   console.log("Received request: " + request.url);
-  response.writeHead(200, {"Content-type":"text/plain"});
-  response.write("Here is some content");
-  response.end();
-  // you can combine response.write and response.end into one line, like this
-  // response.end("Here is some content")
+  fs.readFile("./public" + request.url, function(error, data) {
+    if (error) {
+      response.writeHead(404, {"Content-type":"text/plain"});
+      response.end("Sorry, the page was not found");
+    } else {
+      response.writeHead(200, {"Content-type":"text/html"});
+      response.end(data);
+    }
+  });
 });
 server.listen(port, host, function() {
   console.log("Listening " + host + ":" + port);
